cfad428a68ff845141f65ee0e1820fbfa02d417e
Marcio Silva <marciosilvadrum@gmail.com>
2022-11-15 00:15:24 -0300

    beer list added

--- src/pages/AllBeers.js
+++ src/pages/AllBeers.js
@@ -1,14 +1,49 @@
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function AllBeers() {
+  // console.log(allBeers);
+  const [allBeers, setAllBeers] = useState([]);
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    async function fetchBeers() {
+      const response = await axios.get(
+        "https://ih-beers-api2.herokuapp.com/beers"
+      );
+      setAllBeers(response.data);
+    }
+    fetchBeers();
+  }, [reload]);
+
+  function handleReload() {
+    setReload(!reload);
+  }
+
+  console.log(allBeers);
+
   return (
     <div className="allBeers-container">
       <Header />
 
-      <h1>All Beers</h1>
-
-      <Link to={"/"}>Home</Link>
+      {allBeers.map((beer) => {
+        return (
+          <div key={beer._id} className="beer-box">
+            <div className="img-box">
+              <img
+                src={beer.image_url}
+                alt={`${beer.name}`}
+                style={{ width: 50 }}
+              />
+            </div>
+            <h3>{beer.name}</h3>
+            <h4>{beer.tagline}</h4>
+            <p>{beer.contributed_by}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
